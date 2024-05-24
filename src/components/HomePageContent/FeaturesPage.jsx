c08fce1474bcaadf2d33c7663d71264c2eedd703
atish <kumaratishranjan2003@gmail.com>
2024-05-24 11:34:00 +0530

    change

--- src/components/HomePageContent/FeaturesPage.jsx
+++ src/components/HomePageContent/FeaturesPage.jsx
@@ -2,7 +2,7 @@ const FeaturesPage = () => {
   return (
     <div>
         <div className='text-2xl font-bold text-center'><h1>Features of Complaint Portal</h1></div>
-        <div className='md:flex-row flex mt-10 ml-20 justify-around flex-col-reverse items-center' >
+        <div className='md:flex-row flex mt-10 ml-10 justify-around flex-col-reverse items-center' >
         <ul className='text-1xl font-bold mb-2'>
             <li> Centralized Complaint Tracking </li>
             <li>Good User Experience</li>
@@ -10,7 +10,7 @@ const FeaturesPage = () => {
             <li>Customer Feedback</li>
             <li>Mobile Accessibility</li>
         </ul>
-            <div className='md:w-60 mb-2 w-40 '><img src="./third.png" alt="" /></div>
+            <div className='md:w-60 mb-2 w-40 mr-10'><img src="./third.png" alt="" /></div>
         </div>
     </div>
   )
